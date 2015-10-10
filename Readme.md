78f9cfe67ea030222b06137fb5e7ba59246ce477
FranzZemen <FranzZemen@gmail.com>
2015-10-10 13:56:27 -0500

    docs

--- Readme.md
+++ Readme.md
@@ -1,4 +1,68 @@
-###### BSH Mongo Token ######
-An implementation of BSH Token over MongoDB.  
+# BSH Mongo Token #
+An implementation of bsh-token over MongoDB.  Typically this module would be used with bsh-token.
 
 By default, uses the tokens collection.  Override by calling collection.
+
+## Installation ##
+
+     npm install bsh-mongo-token --save
+
+## Usage ##
+Typically, bsh-mongo-token is used as an implementation for bsh-token:
+
+     var bshToken = require('bsh-token');
+     bshToken.implementation(require('bsh-mongo-token'));
+
+## Dependencies ##
+Uses bsh-mongo-pool, so that needs to be initialized to the appropriate mongo database.
+
+## API Documentation ##
+Typically, the user would not invoke the api methods directly, instead allowing bsh-token to manage the module.  The
+API is documented so that those desiring an alternative implementation such as over MySQL etc., may easily do so.
+
+     /**
+     * Set the mongo collection name for token storage.  Default is 'tokens'.
+     * @param col
+     */
+     module.exports.collection = function (col)
+
+     /**
+     * Supports creation of a token.  Creates a token in the token collection.  The implementation should
+     * persist the token and use the timeout parameters to persist the time at which the token will timeout. Typically
+     * that would be now + timeout, in milliseconds.
+     * @param token The token (alpha numeric, should support size of 1024 although typically it will be 16)
+     * @param context A context string used for third party reporting.
+     * @param user The user (string).  Typically a username.
+     * @param roles An array of strings which are the roles the user is authorized for during the session.
+     * @param tokenSessionTimeout The session timeout
+     * @param tokenFinalTimeout The overall timeout.
+     * @return promise whose success value is the token
+     */
+     module.exports.createToken = function (token, context, user, roles, tokenSessionTimeout, tokenFinalTimeout)
+
+     /**
+     * Update a token's expiration.  Typically called because a client confirms session activity.  Calls the
+     * implementation to do the touch
+     * @param token
+     * @param tokenSessionTimeout optional.  See createToken.
+     * @param tokenFinalTimeout optional.  See createToken.
+     * @returns {*|promise} A promise that resolves to the token
+     */
+     module.exports.touchToken = function (token, tokenSessionTimeout, tokenFinalTimeout)
+
+     /**
+     * Delete a token.  Only deletes if no other delete operation is in progress.  Calls the implementation to actually
+     * perform the delete.
+     * @param token
+     * @returns {*|promise} A promise that evaulates to true if implementation was actually called.  Promise resolves to
+     * false otherwise
+     */
+     module.exports.deleteToken = function (token)
+
+     /**
+     * Delete expired tokens if no deletions are currently ongoing.  Calls the implementation to actually perform the
+     * delete and does not need to manage state.  The implementation shoudl return a promise.
+     * @returns {*|promise} A promise that evaulates to true if implementation was actually called.  False if already
+     * deleting.
+     */
+     module.exports.deleteExpiredTokens = function ()
--- bsh-mongo-token.js
+++ bsh-mongo-token.js
@@ -5,12 +5,28 @@
     var pool = require('bsh-mongo-pool');
     var collection = 'tokens';
 
+    /**
+     * Set the mongo collection name for token storage.  Default is 'tokens'.
+     * @param col
+     */
     module.exports.collection = function (col) {
         log.warn({before:collection, after:col}, 'Changing mongo collection');
         collection = col;
     };
 
 
+    /**
+     * Supports creation of a token.  Creates a token in the token collection.  The implementation should
+     * persist the token and use the timeout parameters to persist the time at which the token will timeout. Typically
+     * that would be now + timeout, in milliseconds.
+     * @param token The token (alpha numeric, should support size of 1024 although typically it will be 16)
+     * @param context A context string used for third party reporting.
+     * @param user The user (string).  Typically a username.
+     * @param roles An array of strings which are the roles the user is authorized for during the session.
+     * @param tokenSessionTimeout The session timeout
+     * @param tokenFinalTimeout The overall timeout.
+     * @return promise whose success value is the token
+     */
     module.exports.createToken = function (token, context, user, roles, tokenSessionTimeout, tokenFinalTimeout) {
         log.trace({token: token, context: context, user: user, roles: roles, tokenSessionTimeout: tokenSessionTimeout, tokenFinalTimeout:tokenFinalTimeout},'createToken');
         var now = Date.now();
@@ -29,7 +45,14 @@
         });
     };
 
-
+    /**
+     * Support touching of a token, or refreshing its timeout.  Pesisted times at which the session and overall timeout
+     * will occur whould be refreshed, typically using now + timeout, in milliseconds.
+     * @param token
+     * @param tokenSessionTimeout
+     * @param tokenFinalTimeout
+     * @return promise whose success value is the token
+     */
     module.exports.touchToken = function (token, tokenSessionTimeout, tokenFinalTimeout) {
         log.trace({token:token, tokenSessionTimeout: tokenSessionTimeout, tokenFinalTimeout:tokenFinalTimeout},'touchToken');
         var now = Date.now();
@@ -45,20 +68,46 @@
             });
     };
 
+    /**
+     * Check if a non expired token supports a role.  The implementation should query for the token, the existence of the role
+     * (as set by createToken) and that it hasn't expired (now is less than the persisted expirations).  The implementation
+     * should allow for an empty, undefined or null role.
+     * @param token
+     * @param role as a string
+     * @return a promise whose success value is the token or null if expired
+     */
     module.exports.checkToken = function (token, role) {
         log.trace({token:token, role:role},'checkToken');
-        var query = {token: token, expiration: {$gt: Date.now()}, finalExpiration: {$gt: Date.now()}, roles :{$all: [role]}};
+        var query = {token: token, expiration: {$gt: Date.now()}, finalExpiration: {$gt: Date.now()}};
+        if (role) {
+            query.roles = {$all: [role]};
+        }
         return pool.db().collection(collection).findOne(query)
             .then(function(tokenData) {
                 return tokenData ? tokenData.token : null;
             });
     };
 
+    /**
+     * Explicitly delete a token.  Typically would be called if a user logs off and for security reasons, the token
+     * should be deleted and not allowed to simply expire.  The implementation should remove or expire the token.
+     * @param token
+     * @return promise whose success value is true
+     */
     module.exports.deleteToken = function (token) {
         log.trace({token:token},'deleteToken');
-        return pool.db().collection(collection).deleteOne({token: token});
+        return pool.db().collection(collection).deleteOne({token: token})
+            .then(function (result) {
+                log.trace('deleteToken done');
+                return true;
+            });
     };
 
+    /**
+     * Deletes any expired tokens.  Might be called periodically (such as by bsh-token, if configured for that). The
+     * implementation should delete all expired tokens.
+     * @return promise whose success value is true
+     */
     module.exports.deleteExpiredTokens = function () {
         log.trace('deleteExpiredTokens');
         pool.db().collection(collection).deleteMany({expiration: {$lt: Date.now()}})
